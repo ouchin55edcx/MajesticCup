e7c047f4947cf813d7538d9d7ca86728f7c1cf15
ouchin55edcx <ouchinmustapha82@gmail.com>
2024-12-08 14:11:36 +0100

    Add necessary dependencies.

---
+++ src/main/java/com/ouchin/MajesticCup/MajesticCupApplication.java
@@ -0,0 +1,13 @@
+package com.ouchin.MajesticCup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MajesticCupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MajesticCupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ouchin/MajesticCup/MajesticCupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ouchin.MajesticCup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MajesticCupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
